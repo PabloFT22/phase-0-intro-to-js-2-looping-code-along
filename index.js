0efeabc083c09bc3c16e5af15a105959703e4300
Pablo Fuentes Tudela <pablo.fuentes.tudela@outlook.com>
2022-09-12 06:46:01 -0400

    commit tree e1abff744818e2b6b22ca1322bec79bebd0cbb66

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    };
+    return messages;
+}
+console.log(messages)
+
+/*function countDown() {
+    for (let countDown = 11; countDown > 0; countDown--){
+        console.log(countDown);
+    }
+}
+console.log(countDown)
+*/
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+    return countDown;
+}
